320bec414bc48a3a7910c8b86e8bc6390aa12e9b
javiuribe <javier.uribe@coitimur.org>
2022-03-25 17:14:37 +0100

    Borrar fichero y añadir ejecutable

--- block.py
+++ block.py
@@ -1,3 +1,9 @@
 import shutil
 import os
+import requests
+r = requests.get('https://raw.githubusercontent.com/javiuribe/bloqueos/main/hosts')
+f = open("hosts",'w')
+f.write(r.text)
+f.close()
 shutil.copyfile(os.getcwd() + "\\hosts", 'C:\\Windows\\System32\\drivers\\etc\\hosts', follow_symlinks=True) #copy src to dst
+os.remove("hosts")
