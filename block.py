cb63617bce8d088f4cf423ecdbd048a4d7e3e8a1
javiuribe <javier.uribe@coitimur.org>
2022-09-05 21:16:34 +0200

    Refactorizar y comentar código

--- block.py
+++ block.py
@@ -1,9 +1,14 @@
+#Crear ejecutable--> pyinstaller --onefile dns_block.py
+
 import shutil
 import os
 import requests
-r = requests.get('https://raw.githubusercontent.com/javiuribe/bloqueos/main/hosts')
+ruta_windows = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+ruta_ubuntu = "/etc/hosts"
+r = requests.get('https://raw.githubusercontent.com/javiuribe/bloqueos/main/hosts') #descargar fichero host
 f = open("hosts",'w')
 f.write(r.text)
 f.close()
-shutil.copyfile(os.getcwd() + "\\hosts", 'C:\\Windows\\System32\\drivers\\etc\\hosts', follow_symlinks=True) #copy src to dst
+shutil.copyfile(
+    os.getcwd() + "\\hosts", ruta_windows, follow_symlinks=True) #sobreescribir fichero host
 os.remove("hosts")
